a3cd7c6b457f36e22eedf9cc44e60a95178b7256
Kromko <igor.romanishkin@gmail.com>
2026-08-26 11:05:58 +0300

    0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
 
+## [0.5] - 2026-08-26
+
+- Fix localization in v13
+
 ## [0.4] - 2026-08-26
 
 - Can no longer attach an item to itself
--- src/module.ts
+++ src/module.ts
@@ -7,104 +7,115 @@ import {
 } from "@7h3laughingman/pf2e-types";
 import { SpecialPredicates, traits, usages } from "./attachments-types";
 
-let currentlyDragging = false;
-Hooks.on("renderActorSheetPF2e", (sheet) => {
-    const { actor, form } = sheet as ActorSheetPF2e<ActorPF2e>;
-    if (!actor.isOfType("npc", "character", "party")) return;
-    form.querySelectorAll(".inventory ul.items > li[data-item-id]").forEach(
-        (li) => {
-            const dataset = (li as HTMLUListElement).dataset;
-            const id = dataset.itemId!;
-            const item = (actor as ActorPF2e).items.get(id);
-            if (!item) return;
+Hooks.once("init", () => {
+    const loc = (() => {
+        if (foundry.utils.isNewerVersion(game.version, 14)) return _loc;
+        // @ts-expect-error "_loc shorthand isn't not present in v13"
+        return game.i18n.format.bind(game.i18n);
+    })();
 
-            li.addEventListener("dragstart", () => dragItem(item));
+    let currentlyDragging = false;
+    Hooks.on("renderActorSheetPF2e", (sheet) => {
+        const { actor, form } = sheet as ActorSheetPF2e<ActorPF2e>;
+        if (!actor.isOfType("npc", "character", "party")) return;
+        form.querySelectorAll(".inventory ul.items > li[data-item-id]").forEach(
+            (li) => {
+                const dataset = (li as HTMLUListElement).dataset;
+                const id = dataset.itemId!;
+                const item = (actor as ActorPF2e).items.get(id);
+                if (!item) return;
 
-            li.addEventListener("dragend", () => {
-                if (!currentlyDragging) return;
-                document
-                    .querySelectorAll(".drag-attach-droppable")
-                    .forEach((e) => e.remove());
-                currentlyDragging = false;
-            });
-        },
-    );
-});
+                li.addEventListener("dragstart", () => dragItem(item));
 
-function dragItem(attachment: ItemPF2e) {
-    if (!attachment) return;
-    if (attachment.isOfType("physical")) {
-        const predicateSource =
-            attachment._source.system.traits.value
-                .map((t) => traits[t])
-                .find((t) => t) ??
-            usages[attachment._source.system.usage?.value ?? ""];
-        if (typeof predicateSource === "undefined" || predicateSource === null)
-            return;
-        const always = predicateSource[0] === SpecialPredicates.Always;
-        const predicate = new game.pf2e.Predicate(predicateSource);
-        const openWindows = Object.values(ui.windows).filter(
-            (w): w is ItemSheetPF2e<PhysicalItemPF2e> =>
-                isItemSheet(w) && w.item?.isOfType("physical"),
+                li.addEventListener("dragend", () => {
+                    if (!currentlyDragging) return;
+                    document
+                        .querySelectorAll(".drag-attach-droppable")
+                        .forEach((e) => e.remove());
+                    currentlyDragging = false;
+                });
+            },
         );
-        if (openWindows.length == 0) {
-            return;
-        }
-        currentlyDragging = true;
-        for (const window of openWindows) {
-            const { item, form } = window;
-            const options = customRollOptions(item);
+    });
 
-            const { allowed, classes, message } = ((item) => {
-                if (item.id === attachment.id)
+    function dragItem(attachment: ItemPF2e) {
+        if (!attachment) return;
+        if (attachment.isOfType("physical")) {
+            const predicateSource =
+                attachment._source.system.traits.value
+                    .map((t) => traits[t])
+                    .find((t) => t) ??
+                usages[attachment._source.system.usage?.value ?? ""];
+            if (
+                typeof predicateSource === "undefined" ||
+                predicateSource === null
+            )
+                return;
+            const always = predicateSource[0] === SpecialPredicates.Always;
+            const predicate = new game.pf2e.Predicate(predicateSource);
+            const openWindows = Object.values(ui.windows).filter(
+                (w): w is ItemSheetPF2e<PhysicalItemPF2e> =>
+                    isItemSheet(w) && w.item?.isOfType("physical"),
+            );
+            if (openWindows.length == 0) {
+                return;
+            }
+            currentlyDragging = true;
+            for (const window of openWindows) {
+                const { item, form } = window;
+                const options = customRollOptions(item);
+
+                const { allowed, classes, message } = ((item) => {
+                    if (item.id === attachment.id)
+                        return {
+                            allowed: false,
+                            classes: ["denied"],
+                            message: "same-item",
+                        };
+                    if (always)
+                        return {
+                            allowed: true,
+                            classes: ["allowed", "unchecked"],
+                            message: "drop-unchecked",
+                        };
+                    if (predicate.test(options))
+                        return {
+                            allowed: true,
+                            classes: ["allowed"],
+                            message: "drop",
+                        };
                     return {
                         allowed: false,
                         classes: ["denied"],
-                        message: "same-item",
+                        message: "no-drop",
                     };
-                if (always)
-                    return {
-                        allowed: true,
-                        classes: ["allowed", "unchecked"],
-                        message: "drop-unchecked",
-                    };
-                if (predicate.test(options))
-                    return {
-                        allowed: true,
-                        classes: ["allowed"],
-                        message: "drop",
-                    };
-                return {
-                    allowed: false,
-                    classes: ["denied"],
-                    message: "no-drop",
-                };
-            })(item);
+                })(item);
 
-            const n = document.createElement("div");
-            n.classList.add("drag-attach-droppable", ...classes);
+                const n = document.createElement("div");
+                n.classList.add("drag-attach-droppable", ...classes);
 
-            const p = document.createElement("p");
-            p.innerHTML = _loc(`pf2e-drag-attach.${message}`);
-            n.appendChild(p);
-            if (allowed) {
-                n.addEventListener("dragover", (event) => {
-                    event.preventDefault();
-                    n.classList.add("drag-over");
-                });
-                n.addEventListener("dragleave", () => {
-                    n.classList.remove("drag-over");
-                });
-                n.addEventListener("drop", async (event) => {
-                    event.preventDefault();
-                    await item.attach(attachment);
-                    await window.render(true);
-                });
+                const p = document.createElement("p");
+                p.innerHTML = loc(`pf2e-drag-attach.${message}`);
+                n.appendChild(p);
+                if (allowed) {
+                    n.addEventListener("dragover", (event) => {
+                        event.preventDefault();
+                        n.classList.add("drag-over");
+                    });
+                    n.addEventListener("dragleave", () => {
+                        n.classList.remove("drag-over");
+                    });
+                    n.addEventListener("drop", async (event) => {
+                        event.preventDefault();
+                        await item.attach(attachment);
+                        await window.render(true);
+                    });
+                }
+                form.querySelector("section.sidebar")?.appendChild(n);
             }
-            form.querySelector("section.sidebar")?.appendChild(n);
         }
     }
-}
+});
 
 function isItemSheet(
     window: foundry.appv1.api.Application<foundry.appv1.api.ApplicationV1Options>,
